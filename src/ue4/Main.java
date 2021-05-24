25c98bc243e4c464154e17450219633be83574ed
Lenni <lennivh24@gmail.com>
2021-05-24 19:39:04 +0200

    U4: A1 finished

--- src/ue4/Main.java
+++ src/ue4/Main.java
@@ -6,5 +6,25 @@ public class Main {
 		Zahlenarray.print(array);
 		System.out.println(Zahlenarray.contains(array, 42));
 		System.out.println(Zahlenarray.contains(array, 100)); // should always be -1
+		
+		final int[] sorted = Zahlenarray.bubblesort(array);
+		Zahlenarray.print(sorted);
+		System.out.println(Zahlenarray.isSorted(sorted));
+		
+		System.out.println(searchAll(sorted));
+	}
+	
+	/**
+	 * Searches all elements in the specified array using binary search.
+	 * Returns the first element that could (wrongfully) not be found.
+	 * Or -1 if all elements were successfully found.
+	 *
+	 * Used for testing.
+	 */
+	private static int searchAll(final int[] array) {
+		for (int i = 0; i < array.length; i++)
+			if (Zahlenarray.binarySearch(array, array[i]) < 0) return i;
+			
+		return -1;
 	}
 }
--- src/ue4/Zahlenarray.java
+++ src/ue4/Zahlenarray.java
@@ -17,13 +17,66 @@ public class Zahlenarray {
 	 * If it is in the list, the amount of comparisons, that were required
 	 * to find the element, is returned.
 	 */
-	public static int contains(int[] hay, int needle) {
+	public static int contains(final int[] hay, final int needle) {
 		int comparisons = 0;
 		
 		for (int i = 0; i < hay.length; i++) {
 			comparisons++;
 			if (needle == hay[i]) return comparisons;
 		}
+		
+		return -1;
+	}
+	
+	public static int[] bubblesort(int[] array) {
+		boolean dirty = false;
+		int tmp;
+		
+		do {
+			dirty = false;
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i] > array[i+1]) {
+					// swap:
+					tmp = array[i+1];
+					array[i+1] = array[i];
+					array[i] = tmp;
+					dirty = true;
+				}
+			}
+		} while (dirty);
+		
+		return array;
+	}
+	
+	public static boolean isSorted(final int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i+1]) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Uses binary search top find a needle faster.
+	 * @return The amount of comparisons, if the needle was found.
+	 * If the array isn't sorted, binary search is not supported and -2 is returned.
+	 * If the element couldn't be found, -1 is returned.
+	 */
+	public static int binarySearch(final int[] haystack, final int needle) {
+		if (!isSorted(haystack)) return -2;
+		
+		int comparisons = 0;
+		
+		int lower = 0, upper = haystack.length;
+		int pos;
+		do {
+			pos = lower + (upper - lower) / 2;
+		
+			comparisons++;
+			
+			if (haystack[pos] == needle) return comparisons;
+			else if (haystack[pos] < needle) lower = pos; // search the upper half
+			else upper = pos; // search the lower half
+		} while (lower < upper);
 		
 		return -1;
 	}
